937627db2d241e84507d268a6bc51526242b9a7d
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2025-03-23 14:40:13 +0530

    Add files via upload

---
+++ 01basic.cpp
@@ -0,0 +1,18 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"Hello coder army"<<endl; #change line to use endl or /n 
+//     cout<<"Hello coder army";
+// }
+ 
+//write  a program to multiplication of two number
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<3*2;
+}
+
+
